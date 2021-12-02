2ea9ff6ecde8c93a8f47a0a0acc43a33f4424846
Jean M. Lescure <jeanmlescure@gmail.com>
2021-12-01 19:42:59 -0600

    fix: make sure SSOUtils are accessible from js

--- dist/utils/index.js
+++ dist/utils/index.js
@@ -20,3 +20,4 @@ SSOUtils.normalizeServerResponse = (res) => {
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {});
 //# sourceMappingURL=index.js.map
+'undefined'!=typeof module&&(module.exports=SSOUtils.default),'undefined'!=typeof window&&(SSOUtils=SSOUtils.default);
